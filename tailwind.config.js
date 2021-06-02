63e2aa5802ea5c25d5e2f37557baad7a31553b0c
Shadab Khan <shadab.2005@gmail.com>
2021-06-02 18:37:51 +0530

    Add tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: ["./**/*.html", "./**/*.tsx"],
   theme: {
     textColor: {
       primary: "#0070f3",

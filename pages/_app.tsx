f3dfbbb163677312f5377bcf71195272491151ec
Shadab Khan <shadab.2005@gmail.com>
2021-06-06 13:41:21 +0530

    Integrate swr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import Link from "next/link";
 import Image from "next/image";
+import { SWRConfig } from "swr";
+import request from "graphql-request";
 
 import "../styles/globals.scss";
 import SearchForm from "../components/SearchForm";
@@ -10,32 +11,43 @@ import SearchForm from "../components/SearchForm";
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   const router = useRouter();
 
+  const swrOptions = {
+    fetcher: (query) =>
+      request(
+        "https://api.thegraph.com/subgraphs/name/protofire/loopring-exchange-v2",
+        query
+      ),
+    refreshInterval: 300000, // refetch every 5 minutes
+  };
+
   return (
-    <main className="w-screen h-screen">
-      <Head>
-        <title>Loopring V2 Explorer</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <header className="bg-loopring w-screen px-4 py-2 text-white">
-        <div className="container h-full w-full lg:w-4/5 m-auto flex md:items-center justify-between flex-col md:flex-row">
-          <div
-            onClick={() => router.push("/")}
-            className="h-full flex items-center"
-          >
-            <Image
-              src="/loopring-white.svg"
-              width="40"
-              height="40"
-              className="h-full"
-              alt="Loopring Logo"
-            />
-            <span className="ml-2 mr-12">LoopringV2 Explorer</span>
+    <SWRConfig value={swrOptions}>
+      <main className="w-screen h-screen">
+        <Head>
+          <title>Loopring V2 Explorer</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <header className="bg-loopring w-screen px-4 py-2 text-white">
+          <div className="container h-full w-full lg:w-4/5 m-auto flex md:items-center justify-between flex-col md:flex-row">
+            <div
+              onClick={() => router.push("/")}
+              className="h-full flex items-center"
+            >
+              <Image
+                src="/loopring-white.svg"
+                width="40"
+                height="40"
+                className="h-full"
+                alt="Loopring Logo"
+              />
+              <span className="ml-2 mr-12">LoopringV2 Explorer</span>
+            </div>
+            <SearchForm className="flex md:w-2/5 mt-2 md:mt-0" />
           </div>
-          <SearchForm className="flex md:w-2/5 mt-2 md:mt-0" />
-        </div>
-      </header>
-      <Component {...pageProps} />
-    </main>
+        </header>
+        <Component {...pageProps} />
+      </main>
+    </SWRConfig>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,61 @@
-import Head from 'next/head'
+import { gql } from "graphql-request";
+import useSWR from "swr";
+
+const FETCH_BLOCKS = gql`
+  query blocks {
+    blocks(first: 10, orderBy: id, orderDirection: desc) {
+      id
+      blockHash
+      timestamp
+    }
+  }
+`;
+
+const FETCH_TXS = gql`
+  query transactions {
+    transactions(first: 10, orderBy: id, orderDirection: desc) {
+      id
+    }
+  }
+`;
 
 export default function Home() {
+  const { data, error } = useSWR(FETCH_BLOCKS);
+  const { data: txsData, error: txsError } = useSWR(FETCH_TXS);
+
+  console.log(data, error);
   return (
     <div>
+      <h1>Loopring Explorer</h1>
+
+      <div className="flex justify-around">
+        <div>
+          <h2>Latest Blocks</h2>
+          <ul>
+            {data &&
+              data.blocks.map((block) => {
+                return (
+                  <li className="w-full flex justify-between items-center">
+                    Block #{block.id} {block.blockHash}
+                  </li>
+                );
+              })}
+          </ul>
+        </div>
+        <div>
+          <h2>Latest Transactions</h2>
+          <ul>
+            {txsData &&
+              txsData.transactions.map((tx) => {
+                return (
+                  <li className="w-full flex justify-between items-center">
+                    Block #{tx.id}
+                  </li>
+                );
+              })}
+          </ul>
+        </div>
+      </div>
     </div>
-  )
+  );
 }

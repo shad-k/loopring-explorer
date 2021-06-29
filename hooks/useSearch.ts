3ce9f54370f83a8562add92648df60a433fe3e2a
Shadab Khan <shadab.2005@gmail.com>
2021-06-30 00:06:20 +0530

    Support multiple search results on search page

--- hooks/useSearch.ts
+++ hooks/useSearch.ts
@@ -9,23 +9,41 @@ const useSearch = (query: string) => {
   const { data: accountData, isLoading: accountIsLoading } = useAccount(query);
 
   const [resultLoaded, setResultLoaded] = React.useState(false);
-  const [redirectLink, setRedirectLink] = React.useState(null);
 
-  // console.log(blockIsLoading, blockData, txIsLoading, txData, accountIsLoading, accountData);
+  const [results, setResults] = React.useState([]);
 
   React.useEffect(() => {
-    if (!blockIsLoading && blockData && blockData.block) {
+    setResultLoaded(false);
+    setResults([]);
+  }, [query]);
+
+  React.useEffect(() => {
+    if (!blockIsLoading && !txIsLoading && !accountIsLoading) {
+      const allResults = [];
+      if (blockData && blockData.block) {
+        allResults.push({
+          type: "block",
+          link: `/block/${blockData.block.id}`,
+          block: blockData.block,
+        });
+      }
+      if (txData && txData.transaction) {
+        allResults.push({
+          type: "tx",
+          link: `/tx/${txData.transaction.id}`,
+          tx: txData.transaction,
+        });
+      }
+      if (accountData && accountData.account) {
+        allResults.push({
+          type: "account",
+          link: `/account/${accountData.account.id}`,
+          account: accountData.account,
+        });
+      }
+
+      setResults(allResults);
       setResultLoaded(true);
-      setRedirectLink(`/block/${blockData.block.id}`);
-    } else if (!txIsLoading && txData && txData.transaction) {
-      setResultLoaded(true);
-      setRedirectLink(`/tx/${txData.transaction.id}`);
-    } else if (!accountIsLoading && accountData && accountData.account) {
-      setResultLoaded(true);
-      setRedirectLink(`/account/${accountData.account.id}`);
-    } else if (!blockIsLoading && !txIsLoading && !accountIsLoading) {
-      setResultLoaded(true);
-      setRedirectLink(null);
     }
   }, [
     blockIsLoading,
@@ -37,14 +55,9 @@ const useSearch = (query: string) => {
     query,
   ]);
 
-  React.useEffect(() => {
-    setResultLoaded(false);
-    setRedirectLink(null);
-  }, [query]);
-
   return {
     loaded: resultLoaded,
-    redirectLink,
+    results,
   };
 };
 
--- pages/search.tsx
+++ pages/search.tsx
@@ -2,16 +2,20 @@ import React from "react";
 import { useRouter } from "next/router";
 import useSearch from "../hooks/useSearch";
 
+import AppLink from "../components/AppLink";
+
 const Search: React.FC<{}> = () => {
   const router = useRouter();
   const searchQuery = router.query.q;
-  const { loaded, redirectLink } = useSearch(searchQuery as string);
+  const { loaded, results } = useSearch(searchQuery as string);
 
   React.useEffect(() => {
-    if (loaded && redirectLink) {
-      router.replace(redirectLink);
+    if (loaded && results.length > 0) {
+      if (results.length === 1) {
+        router.replace(results[0].link);
+      }
     }
-  }, [loaded, redirectLink]);
+  }, [loaded, results]);
 
   if (!loaded) {
     return (
@@ -21,7 +25,7 @@ const Search: React.FC<{}> = () => {
     );
   }
 
-  if (loaded && !redirectLink) {
+  if (loaded && results.length === 0) {
     return (
       <div className="bg-white shadow-custom rounded px-4 py-10 h-40 mt-24 flex items-center justify-between flex-col text-center">
         <h2 className="text-3xl text-indigo-900">No results found</h2>
@@ -29,6 +33,50 @@ const Search: React.FC<{}> = () => {
           We couldn't find any block, transaction or account with ID:&nbsp;
           <span className="font-bold">{searchQuery}</span>
         </div>
+      </div>
+    );
+  }
+
+  if (loaded && results.length > 1) {
+    return (
+      <div className="bg-white shadow-custom rounded px-4 py-10 min-h-table mt-24">
+        <h2 className="text-3xl text-indigo-900">Search Results</h2>
+        <span className="mb-4">
+          We found {results.length} results with ID {searchQuery}
+        </span>
+        {results.map((result) => {
+          let appLinkProps;
+
+          if (result.type === "block") {
+            appLinkProps = {
+              path: "block",
+              block: searchQuery,
+            };
+          } else if (result.type === "tx") {
+            appLinkProps = {
+              path: "transaction",
+              tx: searchQuery,
+            };
+          } else if (result.type === "account") {
+            appLinkProps = {
+              path: "account",
+              accountId: searchQuery,
+            };
+          }
+          return (
+            <AppLink {...appLinkProps}>
+              <div
+                key={`${result.type}-${searchQuery}`}
+                className="my-4 flex items-center justify-between border px-2 py-4 rounded"
+              >
+                <span className="text-lg capitalize">
+                  {result.type} #{searchQuery}
+                </span>
+                <button className="px-4 py-1 rounded-sm">View</button>
+              </div>
+            </AppLink>
+          );
+        })}
       </div>
     );
   }

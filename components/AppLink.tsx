aa7843a82dbf92cf0e1109a15275c0f5a20090c8
Shadab Khan <shadab.2005@gmail.com>
2021-08-28 13:17:50 +0530

    Support token and pair links

--- components/AppLink.tsx
+++ components/AppLink.tsx
@@ -3,32 +3,56 @@ import Link from "next/link";
 
 import { EXPLORER_URL } from "../utils/config";
 
-interface TxLink {
+interface NotTx {
+  tx?: never;
+}
+
+interface NotBlock {
+  block?: never;
+}
+
+interface NotAccount {
+  accountId?: never;
+}
+
+interface NotToken {
+  token?: never;
+}
+
+interface NotPair {
+  pair?: never;
+}
+
+interface TxLink extends NotBlock, NotAccount, NotPair, NotToken {
   path: "transaction";
   tx: string;
-  block?: never;
-  accountId?: never;
 }
 
-interface BlockLink {
+interface BlockLink extends NotTx, NotAccount, NotToken, NotPair {
   path: "block";
   block: string;
-  tx?: never;
-  accountId?: never;
 }
 
-interface AccountLink {
+interface AccountLink extends NotTx, NotBlock, NotToken, NotPair {
   path: "account";
   accountId: string;
-  tx?: never;
-  block?: never;
+}
+
+interface TokenLink extends NotTx, NotBlock, NotAccount, NotPair {
+  path: "token";
+  token: string;
+}
+
+interface PairLink extends NotTx, NotBlock, NotAccount, NotToken {
+  path: "pair";
+  pair: string;
 }
 
 type Props = {
   isExplorerLink?: boolean;
   explorerURL?: string;
   className?: string;
-} & (TxLink | BlockLink | AccountLink);
+} & (TxLink | BlockLink | AccountLink | TokenLink | PairLink);
 
 const makeExplorerURL = (explorerURL: string, link: string): string => {
   return `${explorerURL}${explorerURL.substr(-1) === "/" ? "" : "/"}${link}`;
@@ -39,6 +63,8 @@ const AppLink: React.FC<React.PropsWithChildren<Props>> = ({
   block,
   tx,
   accountId,
+  pair,
+  token,
   isExplorerLink = false,
   explorerURL = EXPLORER_URL,
   children,
@@ -51,6 +77,10 @@ const AppLink: React.FC<React.PropsWithChildren<Props>> = ({
     link = `tx/${tx}`;
   } else if (path === "account") {
     link = `${isExplorerLink ? "address" : "account"}/${accountId}`;
+  } else if (path === "pair") {
+    link = `pair/${pair}`;
+  } else if (path === "token") {
+    link = `token/${token}`;
   }
 
   if (isExplorerLink) {

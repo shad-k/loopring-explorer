437fdec1a5a326f549cbe1e4f30af62f7cbcbd96
Shadab Khan <shadab.2005@gmail.com>
2021-09-14 13:20:15 +0530

    Update subgraph endpoint

--- utils/config.ts
+++ utils/config.ts
@@ -1,4 +1,4 @@
 export const LOOPRING_SUBGRAPH =
-  "https://api.thegraph.com/subgraphs/name/juanmardefago/aasdasd";
+  "https://api.thegraph.com/subgraphs/id/QmUE9Dxniec2VgdHpyEonnYt4yQc9QxFoE9EjqTpAoYRXe";
 export const EXPLORER_URL = "https://etherscan.io/";
 export const USD_PRICE_ENDPOINT = "https://api.0x.org/swap/v1/price?";

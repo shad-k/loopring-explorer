4969f076be546d59e983288006091ca6a1f517b4
Shadab Khan <shadab.2005@gmail.com>
2021-10-17 21:17:50 +0530

    Fix nft card aspect ratio

--- components/NFT.tsx
+++ components/NFT.tsx
@@ -21,8 +21,7 @@ const NFT: React.FC<{ nft: NFTData }> = ({ nft }) => {
     <div
       className="w-full h-full flex flex-col justify-end relative"
       style={{
-        minHeight: 300,
-        minWidth: 300,
+        height: 350,
       }}
     >
       {!isLoaded && (
@@ -35,16 +34,19 @@ const NFT: React.FC<{ nft: NFTData }> = ({ nft }) => {
         <img
           src={image as string}
           alt={name as string}
-          className="flex z-10"
+          className="z-10 object-cover object-center w-full"
           ref={(imageElement) => {
             if (imageElement) {
               imageElement.onload = () => setIsLoaded(true);
             }
           }}
+          style={{
+            height: 300,
+          }}
         />
       )}
       {name && (
-        <div className="bg-white text-loopring-gray px-1 py-2 font-medium text-sm z-10">
+        <div className="bg-white text-loopring-gray px-1 py-2 font-medium text-sm z-10 flex-1 flex items-center">
           {name}
         </div>
       )}

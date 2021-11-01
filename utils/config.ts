96c8b0b3d736a48e737e05c0c14b2eec5f5512a2
Shadab Khan <shadab.2005@gmail.com>
2021-11-01 23:40:46 +0530

    Change subgraph endpoint

--- utils/config.ts
+++ utils/config.ts
@@ -1,5 +1,5 @@
 export const LOOPRING_SUBGRAPH =
-  "https://api.thegraph.com/subgraphs/name/juanmardefago/aasdasd";
+  "https://api.thegraph.com/subgraphs/name/loopring/loopring";
 export const EXPLORER_URL = "https://etherscan.io/";
 export const INFURA_ENDPOINT =
   "https://mainnet.infura.io/v3/bc9124c5abe24dafb21cf55ae12fd15e";

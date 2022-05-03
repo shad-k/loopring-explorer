fe56bab3f639471ce71837835d68d43b5bdb7e8d
Shadab Khan <shadab.2005@gmail.com>
2022-05-04 00:48:00 +0530

    Handle errors in ssr fetches

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,42 @@
-import React from "react";
-import Link from "next/link";
-import numeral from "numeral";
-import { GetServerSideProps } from "next";
+import React from 'react';
+import Link from 'next/link';
+import numeral from 'numeral';
+import { GetServerSideProps } from 'next';
 
-import useBlocks from "../hooks/useBlocks";
-import useTransactions from "../hooks/useTransactions";
-import usePairs from "../hooks/usePairs";
-import client from "../graphql";
+import useBlocks from '../hooks/useBlocks';
+import useTransactions from '../hooks/useTransactions';
+import usePairs from '../hooks/usePairs';
+import client from '../graphql';
 
-import { FETCH_NETWORK_STATS } from "../graphql/queries/home";
-import AppLink from "../components/AppLink";
-import TransactionTableDetails from "../components/transactionDetail/TransactionTableDetails";
-import TableLoader from "../components/TableLoader";
-import USDPriceValue from "../components/USDPriceValue";
+import { FETCH_NETWORK_STATS } from '../graphql/queries/home';
+import AppLink from '../components/AppLink';
+import TransactionTableDetails from '../components/transactionDetail/TransactionTableDetails';
+import TableLoader from '../components/TableLoader';
+import USDPriceValue from '../components/USDPriceValue';
 
-import getTimeFromNow from "../utils/getTimeFromNow";
-import getTrimmedTxHash from "../utils/getTrimmedTxHash";
-import getTokenAmount from "../utils/getTokenAmount";
-import stableCoins from "../utils/stableCoins";
-import getTokenIcon from "../utils/getTokenIcon";
+import getTimeFromNow from '../utils/getTimeFromNow';
+import getTrimmedTxHash from '../utils/getTrimmedTxHash';
+import getTokenAmount from '../utils/getTokenAmount';
+import stableCoins from '../utils/stableCoins';
+import getTokenIcon from '../utils/getTokenIcon';
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const res = await client.query({ query: FETCH_NETWORK_STATS });
+  try {
+    const res = await client.query({ query: FETCH_NETWORK_STATS });
 
-  return {
-    props: { networkStats: res.data },
-  };
+    return {
+      props: { networkStats: res.data },
+    };
+  } catch (error) {
+    console.log(error);
+    return { props: { networkStats: null } };
+  }
 };
 
 export default function Home({ networkStats }) {
-  console.log(networkStats);
   const { data, error, isLoading } = useBlocks();
-  const {
-    data: txsData,
-    error: txError,
-    isLoading: txIsLoading,
-  } = useTransactions();
-  const {
-    data: pairsData,
-    error: pairsError,
-    isLoading: pairsIsLoading,
-  } = usePairs();
+  const { data: txsData, error: txError, isLoading: txIsLoading } = useTransactions();
+  const { data: pairsData, error: pairsError, isLoading: pairsIsLoading } = usePairs();
 
   let avgBlockDetails = React.useMemo(() => {
     if (networkStats && networkStats.blocks.length > 0) {
@@ -54,9 +50,7 @@ export default function Home({ networkStats }) {
       });
       return {
         transactionCount: avgTransactionCount / networkStats.blocks.length,
-        timeBetweenBlocks: `${Math.floor(
-          avgTimeBetweenBlocks / (networkStats.blocks.length * 1000 * 60)
-        )} mins`,
+        timeBetweenBlocks: `${Math.floor(avgTimeBetweenBlocks / (networkStats.blocks.length * 1000 * 60))} mins`,
       };
     }
     return {
@@ -71,47 +65,34 @@ export default function Home({ networkStats }) {
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 lg:gap-16">
           <div className="flex flex-col px-8 py-4 rounded-xl pb-10 border-2 border-loopring-blue dark:border-loopring-dark-gray text-loopring-lightBlue dark:text-white items-center justify-center h-32">
             <span className=" mb-4">Total Transactions</span>
-            <span className="text-3xl flex-1">
-              {numeral(networkStats.proxy.transactionCount).format("0,0")}
-            </span>
+            <span className="text-3xl flex-1">{numeral(networkStats.proxy.transactionCount).format('0,0')}</span>
           </div>
           <div className="flex flex-col px-8 py-2 rounded-xl pb-10 border-2 border-loopring-blue dark:border-loopring-dark-gray text-loopring-lightBlue dark:text-white items-center justify-center h-32">
             <span className=" mb-4">Total Blocks</span>
-            <span className="text-3xl flex-1">
-              {numeral(networkStats.proxy.blockCount).format("0,0")}
-            </span>
+            <span className="text-3xl flex-1">{numeral(networkStats.proxy.blockCount).format('0,0')}</span>
           </div>
           <div className="flex flex-col px-8 py-2 rounded-xl pb-10 border-2 border-loopring-blue dark:border-loopring-dark-gray text-loopring-lightBlue dark:text-white items-center justify-center h-32">
             <span className=" mb-4">Total L2 Accounts</span>
-            <span className="text-3xl flex-1">
-              {data && numeral(networkStats.proxy.userCount).format("0,0")}
-            </span>
+            <span className="text-3xl flex-1">{data && numeral(networkStats.proxy.userCount).format('0,0')}</span>
           </div>
           <div className="flex flex-col px-8 py-2 rounded-xl pb-10 border-2 border-loopring-blue dark:border-loopring-dark-gray text-loopring-lightBlue dark:text-white items-center justify-center h-32">
             <span className=" mb-4">Avg. Block Time</span>
-            <span className="text-3xl flex-1">
-              {avgBlockDetails.timeBetweenBlocks}
-            </span>
+            <span className="text-3xl flex-1">{avgBlockDetails.timeBetweenBlocks}</span>
           </div>
           <div className="flex flex-col px-8 py-2 rounded-xl pb-10 border-2 border-loopring-blue dark:border-loopring-dark-gray text-loopring-lightBlue dark:text-white items-center justify-center h-32">
             <span className=" mb-4">Avg. Txs per Block</span>
             <span className="text-3xl flex-1">
-              {avgBlockDetails.transactionCount &&
-                numeral(avgBlockDetails.transactionCount).format("0,0")}
+              {avgBlockDetails.transactionCount && numeral(avgBlockDetails.transactionCount).format('0,0')}
             </span>
           </div>
           <div className="flex flex-col px-8 py-2 rounded-xl pb-10 border-2 border-loopring-blue dark:border-loopring-dark-gray text-loopring-lightBlue dark:text-white items-center justify-center h-32">
             <span className=" mb-4">Last Block Submitted</span>
-            <span className="text-3xl flex-1">
-              {data && getTimeFromNow(networkStats.blocks[0].timestamp)}
-            </span>
+            <span className="text-3xl flex-1">{data && getTimeFromNow(networkStats.blocks[0].timestamp)}</span>
           </div>
         </div>
       )}
       <div className="w-full mt-8 flex flex-col justify-between">
-        <h2 className="text-2xl font-bold p-2 text-loopring-blue dark:text-loopring-dark-gray">
-          Latest Blocks
-        </h2>
+        <h2 className="text-2xl font-bold p-2 text-loopring-blue dark:text-loopring-dark-gray">Latest Blocks</h2>
         <div className="w-full overflow-x-auto">
           <table className="table-auto w-full">
             <thead className="bg-loopring-blue border border-loopring-blue dark:border-loopring-dark-darkBlue dark:bg-loopring-dark-darkBlue text-white">
@@ -126,21 +107,14 @@ export default function Home({ networkStats }) {
               {data &&
                 data.blocks.map((block) => {
                   return (
-                    <tr
-                      key={block.id}
-                      className="border dark:border-loopring-dark-background ml-2"
-                    >
+                    <tr key={block.id} className="border dark:border-loopring-dark-background ml-2">
                       <td className="p-2 border-b dark:border-loopring-dark-darkBlue whitespace-nowrap">
                         <AppLink path="block" block={block.id}>
                           {block.id}
                         </AppLink>
                       </td>
                       <td className="p-2 border-b dark:border-loopring-dark-darkBlue whitespace-nowrap">
-                        <AppLink
-                          path="transaction"
-                          tx={block.txHash}
-                          isExplorerLink
-                        >
+                        <AppLink path="transaction" tx={block.txHash} isExplorerLink>
                           {getTrimmedTxHash(block.txHash, 15)}
                         </AppLink>
                       </td>
@@ -169,9 +143,7 @@ export default function Home({ networkStats }) {
         </Link>
       </div>
       <div className="w-full mt-8 flex flex-col justify-between">
-        <h2 className="text-2xl font-bold p-2 text-loopring-blue dark:text-loopring-dark-gray">
-          Latest Transactions
-        </h2>
+        <h2 className="text-2xl font-bold p-2 text-loopring-blue dark:text-loopring-dark-gray">Latest Transactions</h2>
         <div className="w-full overflow-x-auto">
           <table className="table-auto w-full">
             <thead className="bg-loopring-blue border border-loopring-blue dark:border-loopring-dark-darkBlue dark:bg-loopring-dark-darkBlue text-white break-none">
@@ -189,10 +161,7 @@ export default function Home({ networkStats }) {
               {txsData &&
                 txsData.transactions.map((tx) => {
                   return (
-                    <tr
-                      className="border dark:border-loopring-dark-background"
-                      key={tx.id}
-                    >
+                    <tr className="border dark:border-loopring-dark-background" key={tx.id}>
                       <td className="p-2 border-b dark:border-loopring-dark-darkBlue whitespace-nowrap">
                         <AppLink path="transaction" tx={tx.id}>
                           {tx.id}
@@ -228,9 +197,7 @@ export default function Home({ networkStats }) {
         </Link>
       </div>
       <div className="w-full mt-8 flex flex-col justify-between">
-        <h2 className="text-2xl font-bold p-2 text-loopring-blue dark:text-loopring-dark-gray">
-          Pairs
-        </h2>
+        <h2 className="text-2xl font-bold p-2 text-loopring-blue dark:text-loopring-dark-gray">Pairs</h2>
         <div className="w-full overflow-x-auto">
           <table className="table-auto w-full">
             <thead className="bg-loopring-blue border border-loopring-blue dark:border-loopring-dark-darkBlue dark:bg-loopring-dark-darkBlue text-white break-none">
@@ -244,10 +211,7 @@ export default function Home({ networkStats }) {
               {pairsData &&
                 pairsData.pairs.map((pair) => {
                   return (
-                    <tr
-                      className="border dark:border-loopring-dark-background"
-                      key={pair.id}
-                    >
+                    <tr className="border dark:border-loopring-dark-background" key={pair.id}>
                       <td className="p-2 border-b dark:border-loopring-dark-darkBlue whitespace-nowrap dark:text-white">
                         <AppLink path="pair" pair={pair.id}>
                           <div className="flex items-center px-4 lg:p-0">
@@ -274,18 +238,12 @@ export default function Home({ networkStats }) {
                       <td className="p-2 pl-36 border-b dark:border-loopring-dark-darkBlue whitespace-nowrap dark:text-white">
                         {stableCoins.includes(pair.token0.symbol) ? (
                           numeral(
-                            getTokenAmount(
-                              pair?.dailyEntities[0]?.tradedVolumeToken0Swap,
-                              pair.token0.decimals
-                            )
-                          ).format("$0.0a")
+                            getTokenAmount(pair?.dailyEntities[0]?.tradedVolumeToken0Swap, pair.token0.decimals)
+                          ).format('$0.0a')
                         ) : stableCoins.includes(pair.token1.symbol) ? (
                           numeral(
-                            getTokenAmount(
-                              pair?.dailyEntities[0]?.tradedVolumeToken1Swap,
-                              pair.token1.decimals
-                            )
-                          ).format("$0.0a")
+                            getTokenAmount(pair?.dailyEntities[0]?.tradedVolumeToken1Swap, pair.token1.decimals)
+                          ).format('$0.0a')
                         ) : (
                           <USDPriceValue
                             token0={pair.token0}
@@ -304,18 +262,12 @@ export default function Home({ networkStats }) {
                       <td className="p-2 border-b dark:border-loopring-dark-darkBlue whitespace-nowrap dark:text-white">
                         {stableCoins.includes(pair.token0.symbol) ? (
                           numeral(
-                            getTokenAmount(
-                              pair?.weeklyEntities[0]?.tradedVolumeToken0Swap,
-                              pair.token0.decimals
-                            )
-                          ).format("$0.0a")
+                            getTokenAmount(pair?.weeklyEntities[0]?.tradedVolumeToken0Swap, pair.token0.decimals)
+                          ).format('$0.0a')
                         ) : stableCoins.includes(pair.token1.symbol) ? (
                           numeral(
-                            getTokenAmount(
-                              pair?.weeklyEntities[0]?.tradedVolumeToken1Swap,
-                              pair.token1.decimals
-                            )
-                          ).format("$0.0a")
+                            getTokenAmount(pair?.weeklyEntities[0]?.tradedVolumeToken1Swap, pair.token1.decimals)
+                          ).format('$0.0a')
                         ) : (
                           <USDPriceValue
                             token0={pair.token0}

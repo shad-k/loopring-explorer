82dfd7ad46564e6d4702bd4735de38f666247482
Shadab Khan <shadab.2005@gmail.com>
2022-05-14 11:47:52 +0530

    Add spinning loaders to pagination arrows while loading

--- components/CursorPagination.tsx
+++ components/CursorPagination.tsx
@@ -1,6 +1,7 @@
 import { QueryResult } from '@apollo/client';
 import React from 'react';
 import { HiOutlineArrowRight as Right, HiOutlineArrowLeft as Left } from 'react-icons/hi';
+import { AiOutlineLoading3Quarters as Loader } from 'react-icons/ai';
 import usePagination from '../hooks/usePagination';
 
 interface CursorPaginationProps {
@@ -24,6 +25,8 @@ const CursorPagination: React.FC<CursorPaginationProps> = ({
 }) => {
   const [page, setPage] = React.useState<number>(1);
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [previousLoading, setPreviousLoading] = React.useState<boolean>(false);
+  const [nextLoading, setNextLoading] = React.useState<boolean>(false);
   const { afterCursor, beforeCursor, fetchNext, fetchPrevious, hasMore } = usePagination(
     data,
     dataKey,
@@ -35,27 +38,39 @@ const CursorPagination: React.FC<CursorPaginationProps> = ({
   const next = async () => {
     setPage((val) => val + 1);
     setIsLoading(true);
+    setNextLoading(true);
     await onNextClick(fetchNext, afterCursor);
     setIsLoading(false);
+    setNextLoading(false);
   };
 
   const previous = async () => {
     setPage((val) => val - 1);
     setIsLoading(true);
+    setPreviousLoading(true);
     await onPreviousClick(fetchPrevious, beforeCursor);
     setIsLoading(false);
+    setPreviousLoading(false);
   };
 
   return (
     <div className="text-white flex items-center justify-center p-4">
-      <button onClick={previous} disabled={page === 1 || isLoading}>
-        <Left size={25} color={page === 1 ? '#656567' : 'white'} />
-      </button>
+      {previousLoading ? (
+        <Loader className="animate-spin" />
+      ) : (
+        <button onClick={previous} disabled={page === 1 || isLoading}>
+          <Left size={25} color={page === 1 ? '#656567' : 'white'} />
+        </button>
+      )}
       <div className="mx-4">Page {page}</div>
-      <button onClick={next} disabled={!hasMore || isLoading}>
-        {' '}
-        <Right size={25} color={!hasMore ? '#656567' : 'white'} />
-      </button>
+      {nextLoading ? (
+        <Loader className="animate-spin" />
+      ) : (
+        <button onClick={next} disabled={!hasMore || isLoading}>
+          {' '}
+          <Right size={25} color={!hasMore ? '#656567' : 'white'} />
+        </button>
+      )}
     </div>
   );
 };

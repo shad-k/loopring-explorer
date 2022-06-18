89b3d7339ce49271b30b5514436f491359e0b0f9
Shadab Khan <shadab.2005@gmail.com>
2022-06-18 13:17:15 +0530

    Add LP token to AMM Exit

--- components/transactionDetail/Remove.tsx
+++ components/transactionDetail/Remove.tsx
@@ -1,10 +1,12 @@
-import React from "react";
-import Image from "next/image";
+import React from 'react';
+import Image from 'next/image';
+import { AiOutlineLoading3Quarters as Loader } from 'react-icons/ai';
 
-import AppLink from "../AppLink";
-import getDateString from "../../utils/getDateString";
-import getTokenAmount from "../../utils/getTokenAmount";
-import getTrimmedTxHash from "../../utils/getTrimmedTxHash";
+import AppLink from '../AppLink';
+import getDateString from '../../utils/getDateString';
+import getTokenAmount from '../../utils/getTokenAmount';
+import getTrimmedTxHash from '../../utils/getTrimmedTxHash';
+import useTokens from '../../hooks/useTokens';
 
 interface IRemoveProps {
   transaction: any;
@@ -12,17 +14,14 @@ interface IRemoveProps {
 }
 
 const Remove: React.FC<IRemoveProps> = ({ transaction, isPending = false }) => {
-  const {
-    block,
-    account,
-    token,
-    data,
-    pool,
-    amount,
-    feeToken,
-    fee,
-    __typename,
-  } = transaction;
+  const { block, account, token, data, pool, amount, feeToken, fee, __typename } = transaction;
+
+  const { data: tokens } = useTokens();
+
+  let tokenDetails = null;
+  if (!token.symbol) {
+    tokenDetails = tokens.find(({ tokenId }) => tokenId === parseInt(token.id));
+  }
 
   return (
     <>
@@ -43,7 +42,7 @@ const Remove: React.FC<IRemoveProps> = ({ transaction, isPending = false }) => {
             <span className="italic">Pending</span>
           ) : (
             <div className="flex items-center ">
-              <Image src={"/green-tick.svg"} height={20} width={20} />{" "}
+              <Image src={'/green-tick.svg'} height={20} width={20} />{' '}
               <span className="ml-2">{getDateString(block.timestamp)}</span>
             </div>
           )}
@@ -56,18 +55,10 @@ const Remove: React.FC<IRemoveProps> = ({ transaction, isPending = false }) => {
       <tr className="border dark:border-loopring-dark-darkBlue">
         <td className="p-2">User Account</td>
         <td>
-          <AppLink
-            path="account"
-            accountId={account.id}
-            address={account.address}
-          >
-            <span className="hidden lg:block">
-              {account.address || account.id}
-            </span>
+          <AppLink path="account" accountId={account.id} address={account.address}>
+            <span className="hidden lg:block">{account.address || account.id}</span>
             <span className="lg:hidden">
-              {account.address
-                ? getTrimmedTxHash(account.address, 10, true)
-                : account.id}
+              {account.address ? getTrimmedTxHash(account.address, 10, true) : account.id}
             </span>
           </AppLink>
         </td>
@@ -77,18 +68,24 @@ const Remove: React.FC<IRemoveProps> = ({ transaction, isPending = false }) => {
         <td>
           <AppLink path="account" accountId={pool.id} address={pool.address}>
             <span className="hidden lg:block">{pool.address || pool.id}</span>
-            <span className="lg:hidden">
-              {pool.address
-                ? getTrimmedTxHash(pool.address, 10, true)
-                : pool.id}
-            </span>
+            <span className="lg:hidden">{pool.address ? getTrimmedTxHash(pool.address, 10, true) : pool.id}</span>
           </AppLink>
         </td>
       </tr>
       <tr className="border dark:border-loopring-dark-darkBlue">
         <td className="p-2">Token Removed</td>
         <td>
-          {getTokenAmount(amount, token.decimals)} {token.symbol}
+          {token.symbol ? (
+            <td>
+              {getTokenAmount(amount, token.decimals)} {token.symbol}
+            </td>
+          ) : tokenDetails ? (
+            <td>
+              {getTokenAmount(amount, tokenDetails.decimals)} {tokenDetails.symbol}
+            </td>
+          ) : (
+            <Loader className="animate-spin" />
+          )}
         </td>
       </tr>
       <tr className="border dark:border-loopring-dark-darkBlue">

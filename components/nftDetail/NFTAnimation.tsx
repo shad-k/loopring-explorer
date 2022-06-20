e7487382c790f1af743b7944a26a4bc7ff218f91
Shadab Khan <shadab.2005@gmail.com>
2022-06-20 14:56:45 +0530

    Fallback to image and support audio animation_url

--- components/nftDetail/NFTAnimation.tsx
+++ components/nftDetail/NFTAnimation.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 
-const NFTAnimation: React.FC<{ animationURL: string; animationType: string }> = ({ animationURL, animationType }) => {
+const NFTAnimation: React.FC<{ animationURL: string; animationType: string; image: string }> = ({
+  animationURL,
+  animationType,
+  image,
+}) => {
   if (animationType?.startsWith('video')) {
-    return <video src={animationURL as string} autoPlay muted controls loop className="z-10 h-full" />;
+    return (
+      <video
+        src={animationURL as string}
+        autoPlay
+        muted
+        controls
+        loop
+        className="z-10 h-full"
+        controlsList="nodownload"
+      />
+    );
   } else if (animationType?.startsWith('text/html')) {
     return (
       <iframe
@@ -13,8 +27,21 @@ const NFTAnimation: React.FC<{ animationURL: string; animationType: string }> = 
         sandbox="allow-scripts allow-same-origin"
       />
     );
+  } else if (animationType?.startsWith('audio')) {
+    return (
+      <div className="z-10 h-full w-[400px] flex flex-col">
+        <img src={image as string} className="object-contain object-center" />
+        <audio
+          src={animationURL as string}
+          controls
+          loop
+          className="w-full rounded-none h-12"
+          controlsList="nodownload"
+        />
+      </div>
+    );
   } else {
-    return <img src={animationURL as string} />;
+    return <img src={image as string} />;
   }
 };
 

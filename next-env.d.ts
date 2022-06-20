5fccb56ba5d71b83c48825c17beb7ab7a02b7285
Shadab Khan <shadab.2005@gmail.com>
2022-06-20 15:18:40 +0530

    Add additional.d.ts and declare model-viewer

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,15 +3,3 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
-
-import React, { useState, DOMAttributes } from 'react';
-
-type CustomElement<T> = Partial<T & DOMAttributes<T> & { children: any }>;
-
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      ['model-viewer']: CustomElement<any>;
-    }
-  }
-}
